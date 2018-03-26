a3abf49f144def1b49f268fa6412f1dee324519c
Noel De Martin <noeldemartin@gmail.com>
2018-03-26 19:22:00 +0200

    Handle options and implement logs command

--- src/up.sh
+++ src/up.sh
@@ -1,1 +1,7 @@
-docker-compose --file "$METAL_HOME/docker-compose.yml" up -d web mongo
+if [ -z "$METAL_DEBUG" ]
+then
+    docker-compose --file "$METAL_HOME/docker-compose.yml" up -d web mongo
+else
+    docker-compose --file "$METAL_HOME/docker-compose.yml" up -d mongo
+    docker-compose --file "$METAL_HOME/docker-compose.yml" up web
+fi
