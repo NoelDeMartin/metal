8cb6690732243222489edffcf94d6eb17759def6
Noel De Martin <noeldemartin@gmail.com>
2018-04-14 17:20:07 +0200

    Rename up, down and ls commands to start, stop and status

--- lib/commands/status.sh
+++ lib/commands/status.sh
@@ -3,8 +3,8 @@ source $METAL_HOME/lib/functions.sh
 list_installed_projects | while read project; do
     printf $project
     if is_project_active $project; then
-        echo " [active @ http://$project.test]"
+        echo " [running @ http://$project.test]"
     else
-        echo " [unactive]"
+        echo " [stopped]"
     fi
 done
